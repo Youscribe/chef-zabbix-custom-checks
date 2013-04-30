34078a961d9f00f537f3fac55667d1782d6d932f
Guilhem Lettron <guilhem@lettron.fr>
2013-04-30 14:33:23 +0200

    manage bin path

--- templates/default/chef-client/chef-client-handler.rb
+++ templates/default/chef-client/chef-client-handler.rb
@@ -24,7 +24,7 @@ module Zabbix
 
       file.write(message)
       file.close()
-      cmd = [File.join(node['zabbix']['bin_dir'], "zabbix_sender"), "--config", File.join(node['zabbix']['etc_dir'], "zabbix_agentd.conf"), "--input-file", file.path]
+      cmd = [File.join(node['zabbix']['home'], "bin", "zabbix_sender"), "--config", File.join(node['zabbix']['etc_dir'], "zabbix_agentd.conf"), "--input-file", file.path]
       Chef::Log.debug "Sending to zabbix: #{message}"
       Chef::Log.debug "Command #{cmd.join(" ")}"
       if RUBY_VERSION < "1.9"
